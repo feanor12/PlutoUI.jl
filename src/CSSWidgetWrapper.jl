d310f66233be5c1df8ae32557262d83343bc2354
Michael Pusterhofer <michael.pusterhofer@ams.com>
2021-05-04 14:37:36 +0200

    add JSWidgetWrapper

--- src/CSSWidgetWrapper.jl
+++ src/CSSWidgetWrapper.jl
@@ -1,4 +1,40 @@
-export CSSWidgetWrapper
+export CSSWidgetWrapper, JSWidgetWrapper
+
+"""
+    JSWidgetWrapper(widget, js)
+
+Inject a script tag where `widget` is set to the first html element of the passed widget.
+
+# Examples
+```
+Change the text color to red when the textfield is focused, otherwise set is to green.
+begin
+    script = \"\"\"
+    widget.onblur = function(){widget.style.color='green'};
+    widget.onfocus = function(){widget.style.color='red'};
+    \"\"\"
+    @bind text JSWidgetWrapper(TextField(),script)
+end
+```
+"""
+struct JSWidgetWrapper
+    widget
+    js
+end
+
+function show(io::IO, m::MIME"text/html", w::JSWidgetWrapper)
+    script = """
+        <script>
+        var widget = currentScript.parentElement.firstElementChild
+        $(w.js)    
+        </script>
+    """
+    show(io,m,w.widget)
+    print(io,script)
+end
+
+Base.get(w::JSWidgetWrapper) = get(w.widget)
+
 
 """
     CSSWidgetWrapper(widget, style::Dict{String,String})
@@ -15,21 +51,7 @@ Generate a text field with 1px wide solid red border.
 @bind text CSSWidgetWrapper(TextField(),Dict("border"=>"1px solid red"))
 ```
 """
-struct CSSWidgetWrapper
-    widget
-    style::Dict{String,String}
+function CSSWidgetWrapper(widget, style::Dict{String,String}) 
+    stylejs = join(["widget.style.$k = '$v';" for (k,v) in style],"\n")
+    JSWidgetWrapper(widget, stylejs)
 end
-
-function show(io::IO, m::MIME"text/html", w::CSSWidgetWrapper)
-    style = join(["widget.style.$k = '$v';" for (k,v) in w.style],"\n")
-    script = """
-        <script>
-        var widget = currentScript.parentElement.firstElementChild
-        $style    
-        </script>
-    """
-    show(io,m,w.widget)
-    print(io,script)
-end
-
-Base.get(w::CSSWidgetWrapper) = get(w.widget)
